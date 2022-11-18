bdf392d0265de55b0ebf40487e24b984566124e6
Deepit15 <95852297+Deepit15@users.noreply.github.com>
2022-11-18 08:39:48 +0530

    updated .pth type

--- main.py
+++ main.py
@@ -18,7 +18,7 @@ def main():
     model = AgingGAN(config)
     trainer = Trainer(max_epochs=config['epochs'], accelerator="gpu",devices=1, auto_scale_batch_size='binsearch')
     trainer.fit(model)
-    torch.save(trainer,'try.pth')
+    torch.save(model.state_dict(),'pretrained_model/state_dict.pth')
 
 
 if __name__ == '__main__':
